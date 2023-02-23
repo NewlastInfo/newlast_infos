eb9b60ad6472a666750dcdb9503123d82c8ad710
zjc <zhangjincan@huishoubao.com.cn>
2023-02-23 18:08:37 +0800

    更新

--- ke_info/ke_info.py
+++ ke_info/ke_info.py
@@ -29,15 +29,19 @@ class KeInfo:
         # url = 'https://36kr.com/information/technology/'
         url = 'https://36kr.com/information/web_news/'
         headers = {
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+
             "accept-encoding": "gzip, deflate, br",
-            "cache-control": "max-age=0",
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36",
+            "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,vi;q=0.7",
+            "origin": "https://36kr.com",
+            "referer": "https://36kr.com/",
+            "sec-ch-ua": "Chromium;v=110, Not A(Brand;v=24, Google Chrome;v=110",
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "Windows",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+
         }
         req_params = dict(
             url=url,
