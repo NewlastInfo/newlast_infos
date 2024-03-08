1e91263e127f74c49e9aa75a65cd8429e2957a87
zjc <zhangjincan@huishoubao.com.cn>
2024-03-08 09:25:58 +0800

    更新转转cokie更新

--- hsb_zz_cookie/zz_cookie_update.py
+++ hsb_zz_cookie/zz_cookie_update.py
@@ -3,9 +3,14 @@
 
 from settings import *
 import pymysql
+import datetime
+import time
+from loguru import logger
+
+logger.add("./spiders/log/{time}.txt", encoding='utf-8')
 
 
-
+@logger.catch
 def update_zz_cookie_count():
     conn = pymysql.connect(host=MYSQL_HOST, user=MYSQL_USER, port=MYSQL_PORT, password=MYSQL_PASSWORD,
                            database=MYSQL_DATABASES)
@@ -14,3 +19,12 @@ def update_zz_cookie_count():
     cursor.execute(update_sql)
     conn.commit()
     conn.close()
+
+
+while True:
+    hour = datetime.datetime.now().hour
+    if hour == 9:
+        update_zz_cookie_count()
+        time.sleep(60 * 60)
+    else:
+        time.sleep(60 * 10)
