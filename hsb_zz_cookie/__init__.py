e487395ce398cbc46667b8b179c0484eeb05515c
zjc <zhangjincan@huishoubao.com.cn>
2024-03-08 09:18:01 +0800

    新增转转更新

--- hsb_zz_cookie/__init__.py
+++ hsb_zz_cookie/__init__.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
-ss = ['https://www.cnet.com/tech/mobile/ios-17-cheat-sheet-what-you-need-to-know-about-the-iphone-update/',
-      'https://www.cnet.com/tech/mobile/ios-17-cheat-sheet-what-you-need-to-know-about-the-iphone-update/',
-      'https://www.cnet.com/personal-finance/banking/advice/3-ways-to-save-for-your-next-vacation-while-savings-rates-are-still-high/',
-      'https://www.cnet.com/tech/mobile/ios-17-cheat-sheet-what-you-need-to-know-about-the-iphone-update/',
-      'https://www.cnet.com/tech/mobile/ios-17-cheat-sheet-what-you-need-to-know-about-the-iphone-update/']
